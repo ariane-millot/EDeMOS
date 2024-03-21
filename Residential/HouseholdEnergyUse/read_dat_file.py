ae87fa06f625f861ce4748870bb3e164222d573b
Millot <amillot@ic.ac.uk>
2024-03-21 00:03:27 +0000

    Rename columns automatically

--- Residential/HouseholdEnergyUse/read_dat_file.py
+++ Residential/HouseholdEnergyUse/read_dat_file.py
@@ -15,24 +15,25 @@ filename = 'ZMHR71FL'
 # This currently needs to be found before running this script by inspecting the files in a text editor
 N = 167
 
-# Read the .DCT file which specifices which charaters of the .DAT file enocde which columns
-dct = read_fwf(data_folder+filename+'.DCT',skiprows=2,header=None,nrows=N)
+# Read the .DCT file which specifies which characters of the .DAT file encode which columns
+# replace - with 3 spaces in the .dct file before running the code
+dct = read_fwf(data_folder+filename+'.DCT', skiprows=2, header=None, nrows=N)
+print('Read .dct file: shape =', dct.shape)
 # Read the names of the columns from the .DO file
-print('Read .dct file: shape =',dct.shape)
-do = read_fwf(data_folder+filename+'.DO',skiprows=2,header=None,nrows=N)
-print('Read .do file: shape =',do.shape)
+do = read_fwf(data_folder+filename+'.DO', skiprows=2, header=None, nrows=N)
+print('Read .do file: shape =', do.shape)
 
 # Append the column names to the character positions
 dct = dct.merge(do,left_on=1,right_on=2,how='left')
 
 # Make array of column names
-cols = np.array(dct[dct.columns[[4,5]]])
+cols = np.array(dct[dct.columns[[4, 5]]])
 
 # Reduce the character positions by 1 because python starts counting from 0 (not from 1).
-cols[:,0]-=1
+cols[:, 0]-=1
 
 # Now read .DAT file containing encoded data
-data = read_fwf(data_folder+filename+'.DAT',header=None,colspecs=list(map(tuple,cols)))
+data = read_fwf(data_folder+filename+'.DAT',header=None, colspecs=list(map(tuple,cols)))
 
 # Get rid of any duplicated quotation marks in column names
 data.columns = np.char.strip(dct.iloc[:,-1].to_numpy(str),'"')
@@ -59,10 +60,26 @@ cols_required = ["Sample strata for sampling errors",
                  "Wealth index for urban/rural",
                  "Wealth index factor score for urban/rural (5 decimals)"]
 
+# Define a dictionary to map old names to new names
+old_to_new_names = {
+    "Has electricity": "Electricity",
+    "Has radio": "Radio",
+    "Has mobile telephone": "Mobile telephone",
+    "Has television": "Television",
+    "Has refrigerator": "Refrigerator",
+    "Has a computer": "Computer"
+}
+
 # Write the data for useful columns only in .csv format
 outfile = data_folder+'household_data.csv'
-data[cols_required].to_csv(outfile,index=False)
-print('Written to ',outfile)
+data = data[cols_required].rename(columns=old_to_new_names)
+
+
+# Write the data for useful columns only in .csv format
+outfile = data_folder+'household_data.csv'
+# data[cols_required].to_csv(outfile,index=False)
+data.to_csv(outfile,index=False)
+print('Written to ', outfile)
 # N.B. The data will still need to be translated from numeric values to their meaning
 # using the information in the .DO file
 
