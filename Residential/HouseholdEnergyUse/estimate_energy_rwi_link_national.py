d320e8b0d597a7fd6e6dafe7d475705f125eed3a
ariane-millot <61950310+ariane-millot@users.noreply.github.com>
2024-08-12 19:22:38 +0100

    Graph update
    Graph improvements and elasticity update

--- Residential/HouseholdEnergyUse/estimate_energy_rwi_link_national.py
+++ Residential/HouseholdEnergyUse/estimate_energy_rwi_link_national.py
@@ -1,15 +1,18 @@
 from pandas import read_csv
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import pathlib
+import seaborn as sns
 
 from estimate_energy_perhh_DHS import compute_energy_perhh_DHS
 
 data_folder = '../Data/DHSSurvey/'
 figures_folder = '../Figure/'
+make_figure = True
 
-elas = 1  # choose elasticity value for the country
+elas = 0.35  # choose elasticity value for the country
 compute_energy_perhh_DHS(elas=elas)  # Run the script to assess energy consumption of households in the DHS dataset
 
 infile = data_folder + 'household_data.csv'  # Read file containing data from DHS survey of households
@@ -24,11 +27,18 @@ province = dataDHS['Province'].to_numpy(int)
 region_type = ['urban', 'rural']
 
 # Parameters for the graphs
-xl = np.array([-2, 3.99])  # Limits for x-axis of plots (wealth index)
-bx = np.arange(xl[0], xl[1], 0.4)  # Bins for histograms on x-axis
+min_wealth = np.floor(wealth_index.min())
+max_wealth = np.ceil(wealth_index.max())
+print(min_wealth, max_wealth)
+# xl = np.array([-2, 2])  # Limits for x-axis of plots (wealth index)
+xl = np.array([min_wealth, max_wealth])  # Limits for x-axis of plots (wealth index)
+bx = np.arange(xl[0], xl[1], 0.1)  # Bins for histograms on x-axis
 yl = np.array([0, 2999])  # Limits for y-axis on scatter plots (energy use)
+al = 0.3  # Alpha value for points in figures
+x_axis_cells_results_option = False
+letters = ['(a)', '(b)']
 
-hh_cells_results_available = False  # Change to False if this is the first run of the analysis
+hh_cells_results_available = True  # Change to False if this is the first run of the analysis
 if hh_cells_results_available is True:
     infile = '../../data.csv'  # Read file containing the estimated mean wealth index ("rwi") of each hexagon on map
     data_hh = read_csv(infile)
@@ -37,19 +47,12 @@ if hh_cells_results_available is True:
     energy_demand = np.zeros((2, N))  # Array to save energy demand estimates
     # print(energy_demand)
 
-Nb = 20  # Number of points to used to map wealth index to energy use
-
-
-make_figure = True
-if make_figure:
-    al = 0.6  # Alpha value for points in figures
 
 outputfile = '../Data/DHSSurvey/dataDHS_group.csv'
 # Create an empty DataFrame
 data_output = pd.DataFrame()
 
-
-Nb = 100
+Nb = 100  # Number of points to used to map wealth index to energy use
 
 for i in range(2):
 
@@ -63,12 +66,10 @@ for i in range(2):
 
     Nh = rwi_DHS.size
     o = np.argsort(rwi_DHS)  # Find indices that order the survey households by ascending rwi
-    if hh_cells_results_available is True:
-        # Create filter to identify map regions (hexagons) of the relevant type and province
-        include = (data_hh[column_name] > 0)
 
     # Create Nb overlapping groups of points with ascending average rwi
     Ng = min(int(Nh / 4), 1000)  # define the size of each group of DHS data points
+    print(Ng)
     step = int((Nh - Ng) / (Nb - 1))  # define the increment between each group
     group = np.zeros((Nb, Ng), dtype=int)
     for k in range(Nb):  # creates overlapping group as step < Ng
@@ -93,77 +94,86 @@ for i in range(2):
     data_output['w_group_' + region_type[i]] = w_group
 
     if hh_cells_results_available is True:
+        # Create filter to identify map regions (hexagons) of the relevant type and province
+        include = (data_hh[column_name] > 0)
+
         # Allocate estimated average household energy demand to hexagon subregions
         # by interpolating between running average of survey data
         energy_demand[i, include] = np.interp(data_hh['rwi'][include], rwi_group, eu_group)
 
     if make_figure:
-        fig = plt.figure(figsize=(14, 8))
-        plt.subplots_adjust(left=0.06, bottom=0.07, right=0.94, top=0.96, hspace=0, wspace=0.03)
+        palette = sns.color_palette()
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8.5, 5),
+                                       gridspec_kw={'height_ratios': [1, 2]})
 
-        axk = plt.subplot(4, 1, 4)
-        axk.set_axis_off()
-        axk.set_xlim(0, 1)
-        axk.set_ylim(0, 1)
-        xk = 0.1
-        dxk = 0.25
-        yk = 0.3
-
-    if make_figure:
         # First subplot
-        ax1 = plt.subplot2grid((4, 1), (0, 0), 1, 1)
-
         # Plots a weighted density histogram of rwi DHS values for survey households.
-        plt.hist(rwi_DHS, bins=bx, weights=w, density=True, alpha=al, edgecolor='b', facecolor='None')
-        plt.xlim(xl)
-        plt.xticks([])
-        plt.ylim(0, 1.7)
+        ax1.hist(rwi_DHS, bins=bx, weights=w/w.sum()*100, density=False, edgecolor=palette[0],
+                 facecolor='None', label='Survey households')
+        # sns.kdeplot(x=rwi_DHS, weights=w/w.sum()*100, color=palette[0], ax=ax1, label='Survey households')
+        ax1.set_xlim(xl)
+        ax1.set_xticks([])
+        # ax1.set_ylim(0, 80)
+        ax1.set_ylabel('Household\ndensity (%)\n\n')
 
         if hh_cells_results_available is True:
+            if x_axis_cells_results_option is True:
+                # option to choose a different x-axis
+                # Parameters for the graphs
+                min_rwi = np.floor(data_hh['rwi'][include].min())
+                max_rwi = np.ceil(data_hh['rwi'][include].max())
+                print(min_rwi, max_rwi)
+                xl = np.array([min_rwi, max_rwi])  # Limits for x-axis of plots (wealth index)
+                ax1.set_xlim(xl)
             # Plots a density histogram of rwi values for our dataset with orange shading.
             ax1.hist(data_hh['rwi'][include],
-                     bins=bx, weights=data_hh[column_name][include],
-                     density=True, edgecolor='None', facecolor='orange')
+                     bins=bx, weights=data_hh[column_name][include]/data_hh[column_name][include].sum()*100,
+                     density=False, edgecolor=palette[4], facecolor='None', label='Dataset households')
+            # sns.kdeplot(x=data_hh['rwi'][include], weights=data_hh[column_name][include]/d
+            # ata_hh[column_name][include].sum()*100,
+            #             color=palette[4], ax=ax1, label='Dataset households')
+        ax1.legend()
 
         # Second subplot
-        ax2 = plt.subplot2grid((4, 1), (1, 0), 2, 1)
-        plt.xlim(xl)
-        # plt.ylim(yl)
+        ax2.set_xlim(xl)
+        # ax2.set_ylim(yl)
+        scaling = 3
         # Plots a scatter plot of rwi vs. energy use for survey households from DHS data with light green dot
-        plt.scatter(rwi_DHS, eu, s=w, c='c', alpha=al, edgecolors='None')
-        plt.text(xl[0], yl[-1], '\n  ', va='top')
-        # Add the average rwi vs energy use for the region
-        plt.plot(rwi_DHS.mean(), eu.mean(), '+', color='g', ms=20)
-        plt.ylabel('Household annual energy consumption, kWh')
-        plt.xlabel('Wealth index')
+        ax2.scatter(rwi_DHS, eu, s=w*5*scaling, alpha=al, c=[palette[0]], edgecolors='None', label='Survey households')
+        ax2.text(xl[0], yl[-1], '\n  ', va='top')
+        # # Add the average rwi vs energy use for the region
+        # ax2.plot(rwi_DHS.mean(), eu.mean(), '+', color=palette[2], ms=20)
+        ax2.set_ylabel('Household annual\nenergy consumption (kWh)')
+        ax2.set_xlabel('Wealth index')
 
         # Scatter plot of national values
-        plt.plot(rwi_group, eu_group, '-', color='grey', alpha=0.3)
+        ax2.plot(rwi_group, eu_group, '-', color='grey', alpha=0.3)
         # Scatter plot of group values
-        plt.scatter(rwi_group, eu_group, s=w_group, alpha=al, marker='d', c='None', edgecolors='b')
+        ax2.scatter(rwi_group, eu_group, s=w_group*10*scaling, marker='d', alpha=al*2, c='None', edgecolors=palette[1], label='Artificial groups of survey households')
+        ax2.get_yaxis().set_major_formatter(
+            matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
         if hh_cells_results_available is True:
             # Scatter plot of rwi values for our dataset vs energy demand for subregions with orange dots
-            plt.scatter(data_hh['rwi'][include], energy_demand[i, include], alpha=al, marker='o', s=1, c='orange',
-                        edgecolors='None')
+            ax2.scatter(data_hh['rwi'][include], energy_demand[i, include], s=5*scaling, alpha=al, marker='o', c=[palette[4]],
+                        edgecolors=palette[4], label='Dataset households')
+        ax2.legend()
 
-    if make_figure:
-        # Make key to symbols
-        axk.text(0, yk, 'Key to symbols:', va='center')
-        axk.plot(xk, yk, 'o', mfc='c', mec='None', alpha=al)
-        axk.text(xk, yk, '  Survey households', va='center')
-        xk += dxk
-        axk.plot(xk, yk, 'd', mfc='None', mec='b', alpha=al)
-        axk.text(xk, yk, '  Artifical groups of survey households', va='center')
-        xk += dxk
+        # Put a legend below current axis
+        handles, labels = ax2.get_legend_handles_labels()
+        ax2.legend(handles, labels, title='Key to symbols', loc='lower center',
+                   bbox_to_anchor=(0.5, -0.6),
+                   fancybox=False, ncol=4, facecolor='white', framealpha=1)
+
         if hh_cells_results_available is True:
-            axk.plot(xk, yk, 's', mec='None', mfc='orange', alpha=al)
-            axk.text(xk, yk, '  Real groups of households from map', va='center')
-
-        outfile = f'household_groups_{region_type[i].lower()}_withweight_elas{elas}_woWorldpopData.png'
+            outfile = f'household_groups_{region_type[i].lower()}_withweight_elas{elas}_withWorldpopData_newvalues.png'
+        else:
+            outfile = f'household_groups_{region_type[i].lower()}_withweight_elas{elas}_woWorldpopData_newvalues.png'
         pathlib.Path(figures_folder).mkdir(exist_ok=True)
-        plt.savefig(figures_folder + outfile)
+        fig.suptitle(f'{letters[i]} {region_type[i].capitalize()}')
+        plt.tight_layout()
+        plt.savefig(figures_folder + outfile, dpi=300)
         print('Created ' + outfile)
-        plt.show()
+        # plt.show()
         plt.close()
 
     # # Add the assessed energy use in the output file
