8d0fa012b271eaf145cc30269defe56c065bc026
ariane-millot <61950310+ariane-millot@users.noreply.github.com>
2025-12-12 16:12:56 +0000

    Update with new names

--- size_file_reduction.py
+++ size_file_reduction.py
@@ -5,7 +5,7 @@ import geopandas as gpd
 import sys
 import os
 current_dir = os.path.abspath(os.getcwd())
-if os.path.basename(current_dir) != 'DemandMappingZambia':
+if os.path.basename(current_dir) != 'EDeMOS':
   sys.path.insert(0, os.path.dirname(current_dir))
   os.chdir('..')
   print(f'Move to {os.getcwd()}')
@@ -15,25 +15,26 @@ ROOT_DIR = os.path.abspath(os.curdir)
 in_path = ROOT_DIR
 out_path = ROOT_DIR + "/Outputs"
 
-file = gpd.read_file(out_path + "\\" + f'total_demand.geojson')  # , driver='GeoJSON', index=False)
+file = gpd.read_file(out_path + "\\" + f'total_electricity_consumption_Zambia.gpkg')  # , driver='GeoJSON', index=False)
 print(file.columns)
 
 list_columns_tokeep = ['h3_index', 'geometry',
                      'n0', 'n1', 'n2', 'n3', 'n4', 'n5',
+                     'NAME_1',
                      'locationWP',
                      'HH_total',
-                     'population',
+                     'pop_total',
                      'HHwithAccess',
-                     'res_Bui',
+                     #'res_Bui',
                      'total_employee',
-                     'ResEnergy_kWh_meth2',
-                     'ResEnergy_kWh_meth2_scaled',
-                     'SEn_kWh',
-                     'IndEnergy_GWh',
+                     'res_elec_kWh_meth2',
+                     'res_elec_kWh_meth2_scaled',
+                     'ser_elec_kWh_final',
+                     'ind_elec_GWh',
                      # add mining output?
                      ]
 
 file_website = file[list_columns_tokeep]
 # rename column?
 # file_website.head(3)
-file_website.to_file(out_path + "\\" + f'total_demand_web.geojson', driver='GeoJSON', index=False)
+file_website.to_file(out_path + "\\" + f'total_elec_consumption_web.gpkg', driver='GPKG') #, index=False)
